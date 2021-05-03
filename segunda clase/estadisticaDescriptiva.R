18d99c92265be820abc4f4ce66adf4dbb00076b3
Andres Felipe <afbeltranr@unal.edu.co>
2021-05-03 11:15:52 -0500

    .

---
+++ segunda clase/estadisticaDescriptiva.R
@@ -0,0 +1,16 @@
+exp <- c(47,52,52,57,58,58,60,65,66,66,71,71,72,73,96)
+median(exp)
+summary(exp)
+library(downloader)
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+
+library(readr)
+msleep_ggplot2 <- read_csv("msleep_ggplot2.csv")
+View(msleep_ggplot2)
+
+# estudiantes <- c('Carlos Amado','Daniel Martinez','Gabriel Ballen','Isabella Blanco','Javier Suesca','Juliana Maldonado','Juliana Penaloza','Laura Cardenas','Leidy Bohorquez','Magda Isabel','Wendy Gonzalez', 'Lizeth Pineda')
+ 
+sample(estudiantes,1) 
+  
