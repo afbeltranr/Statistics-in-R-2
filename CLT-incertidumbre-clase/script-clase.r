542997072523de1d32bdc5f2512918f568bb8e0e
Andres Felipe <afbeltranr@unal.edu.co>
2021-05-31 09:02:12 -0500

    script

---
+++ CLT-incertidumbre-clase/script-clase.r
@@ -0,0 +1,16 @@
+library(chemometrics)
+data(glass)
+CaO <- glass[,'CaO']
+
+set.seed(1)
+n <- 1000 
+N <- 12
+averages50 <- vector("numeric", n)
+for(i in 1:n){
+  
+ 	muestra <- sample(caO, N)
+}
+
+
+
+
